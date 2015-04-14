bdff657178a4ed3c2438b8dce3a7fdd62d466b4d
Rodrigo Luna <rluna@artedigital-mx.com>
2015-04-14 16:41:52 -0700

    change

--- index.php
+++ index.php
@@ -13,7 +13,13 @@
   </style>
   <script>
   $(function() {
-    $( "#draggable" ).draggable();
+    $( "#draggable" ).draggable({
+      cancel: "a.ui-icon", // clicking an icon won't initiate dragging
+      revert: "invalid", // when not dropped, the item will revert back to its initial position
+      containment: "document",
+      helper: "clone",
+      cursor: "move"
+    });
     $( "#droppable" ).droppable({
       drop: function( event, ui ) {
         $( this )
